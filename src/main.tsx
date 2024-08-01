cd010f704410e06e2964078722a6fc700cb344d6
robenend <roberaendale2@gmail.com>
2024-08-01 03:20:05 +0300

    logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './App.css'
-import { ThemeProvider } from './components/theme/theme-provider.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import './App.css';
+import { ThemeProvider } from './components/theme/theme-provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
